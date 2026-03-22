112617a25fe42b7aaecdb82e05e4ca7e04f16704
janviramoliya <janviramoliya27@gmail.com>
2026-03-22 20:25:19 +0530

    Add authorize roles guard
    Add authorize roles guard

---
+++ src/guards/roles/roles.guard.ts
@@ -0,0 +1,24 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Observable } from 'rxjs';
+import { Role } from '../../generated/prisma/enums.js';
+import { ROLES_KEY } from './roles.decorator.js';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (!requiredRoles) return true;
+
+    const request = context.switchToHttp().getRequest();
+
+    return true;
+  }
+}
